b8fd4c6110c7ce624817449ee396d6bdd3cc3253
Jason Wong <jasonkwh@gmail.com>
2016-08-19 14:07:52 +1000

    Simple gameObject destruction

---
+++ Assets/Scripts/DestroySphere.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroySphere : MonoBehaviour {
+
+	void OnCollisionEnter (Collision col) {
+		if (col.gameObject.name == "Pots") {
+			Destroy (this.gameObject);
+		}
+	}
+}
