0da4aa5f61b755bee6abc00c8825065933fc8baa
Jason Wong <jasonkwh@gmail.com>
2016-08-22 15:20:02 +1000

    Trails auto-disappear

---
+++ Assets/Scripts/DestroyTrails.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyTrails : MonoBehaviour {
+
+	public float destroyTime;
+	
+	// Update is called once per frame
+	void Update () {
+		Destroy (this.gameObject, destroyTime);
+	}
+}
