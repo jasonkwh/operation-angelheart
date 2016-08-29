6c31dd7bd51a4f7f374a895e6a7f2ed96ea653a6
Jason Wong <jasonkwh@gmail.com>
2016-08-29 16:39:56 +1000

    Fix UI scale

---
+++ Assets/Scripts/HealthPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPlayer : MonoBehaviour {
+
+	public GameObject pot;
+	public RectTransform bar;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 pos = pot.transform.position;  // get the game object position
+		Vector2 viewportPoint = Camera.main.WorldToViewportPoint(pos);  //convert game object position to VievportPoint
+
+		// set MIN and MAX Anchor values(positions) to the same position (ViewportPoint)
+		bar.anchorMin = viewportPoint;  
+		bar.anchorMax = viewportPoint; 
+	}
+}
