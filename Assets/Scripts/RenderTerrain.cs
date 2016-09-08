a4c22237491f31575454425952dccd626a578384
Jason Wong <jasonkwh@gmail.com>
2016-09-08 17:04:15 +1000

    Rewrote terrain script
    Now the terrains are pre-designed and not randomly generated; Prefab
    name of terrain must strictly follows its format: terrain_x_z; "x" is
    the horizontal value and "z" for vertical; Scale is 70x110 and must
    store in Resources folder

--- Assets/Scripts/RenderTerrain.cs
+++ Assets/Scripts/RenderTerrain.cs
@@ -12,6 +12,7 @@ public class RenderTerrain : MonoBehaviour {
 	private GameObject terrain01n;
 	//private float dist;
 	//private float maxDist = 200f;
+	private bool mainTerrain;
 
 	// Use this for initialization
 	void Start () {
@@ -26,23 +27,60 @@ public class RenderTerrain : MonoBehaviour {
 		//dist = Vector3.Distance (pot.transform.position, transform.position);
 
 		//Instantiate new terrains
-		if (pot.transform.position.x > transform.position.x) {
+		//awful destruction
+		/*if ((pot.transform.position.x > transform.position.x) && (pot.transform.position.x < transform.position.x + 1)) {
 			terrainRender(terrain10, ("terrain_" + (parameterX + 1).ToString () + "_" + parameterZ.ToString ()), new Vector3 ((transform.position.x + transform.localScale.x), 0, transform.position.z));
 		}
-		if (pot.transform.position.x < transform.position.x) {
+		if ((pot.transform.position.x < transform.position.x) && (pot.transform.position.x > transform.position.x - 1)) {
 			terrainRender(terrain10n, ("terrain_" + (parameterX - 1).ToString () + "_" + parameterZ.ToString ()), new Vector3 ((transform.position.x - transform.localScale.x), 0, transform.position.z));
 		}
-		if (pot.transform.position.z > transform.position.z) {
+		if ((pot.transform.position.z > transform.position.z) && (pot.transform.position.z < transform.position.z + 1)) {
 			terrainRender(terrain01, ("terrain_" + parameterX.ToString () + "_" + (parameterZ + 1).ToString ()), new Vector3 (transform.position.x, 0, (transform.position.z + transform.localScale.z)));
 		}
-		if (pot.transform.position.z < transform.position.z) {
+		if ((pot.transform.position.z < transform.position.z) && (pot.transform.position.z > transform.position.z - 1)) {
 			terrainRender(terrain01n, ("terrain_" + parameterX.ToString () + "_" + (parameterZ - 1).ToString ()), new Vector3 (transform.position.x, 0, (transform.position.z - transform.localScale.z)));
+		}*/
+		//check the pot is on main terrain or not
+		if ((pot.transform.position.x < (transform.position.x + (transform.localScale.x / 2))) && (pot.transform.position.x > (transform.position.x - (transform.localScale.x / 2))) && (pot.transform.position.z < (transform.position.z + (transform.localScale.z / 2))) && (pot.transform.position.z > (transform.position.z - (transform.localScale.z / 2)))) {
+			mainTerrain = true;
+		} else {
+			mainTerrain = false;
 		}
 
-		//Destroy out of distance
+		if (mainTerrain == true) {
+			if (pot.transform.position.x > transform.position.x) {
+				terrainRender(terrain10, ("terrain_" + (parameterX + 1).ToString () + "_" + parameterZ.ToString ()), new Vector3 ((transform.position.x + transform.localScale.x), 0, transform.position.z));
+			}
+			if (pot.transform.position.x < transform.position.x) {
+				terrainRender(terrain10n, ("terrain_" + (parameterX - 1).ToString () + "_" + parameterZ.ToString ()), new Vector3 ((transform.position.x - transform.localScale.x), 0, transform.position.z));
+			}
+			if (pot.transform.position.z > transform.position.z) {
+				terrainRender(terrain01, ("terrain_" + parameterX.ToString () + "_" + (parameterZ + 1).ToString ()), new Vector3 (transform.position.x, 0, (transform.position.z + transform.localScale.z)));
+			}
+			if (pot.transform.position.z < transform.position.z) {
+				terrainRender(terrain01n, ("terrain_" + parameterX.ToString () + "_" + (parameterZ - 1).ToString ()), new Vector3 (transform.position.x, 0, (transform.position.z - transform.localScale.z)));
+			}
+			if ((pot.transform.position.x > transform.position.x) && (pot.transform.position.z > transform.position.z)) {
+				terrainRender(terrain10, ("terrain_" + (parameterX + 1).ToString () + "_" + (parameterZ + 1).ToString ()), new Vector3 ((transform.position.x + transform.localScale.x), 0, (transform.position.z + transform.localScale.z)));
+			}
+			if ((pot.transform.position.x > transform.position.x) && (pot.transform.position.z < transform.position.z)) {
+				terrainRender(terrain10, ("terrain_" + (parameterX + 1).ToString () + "_" + (parameterZ - 1).ToString ()), new Vector3 ((transform.position.x + transform.localScale.x), 0, (transform.position.z - transform.localScale.z)));
+			}
+			if ((pot.transform.position.x < transform.position.x) && (pot.transform.position.z > transform.position.z)) {
+				terrainRender(terrain10, ("terrain_" + (parameterX - 1).ToString () + "_" + (parameterZ + 1).ToString ()), new Vector3 ((transform.position.x - transform.localScale.x), 0, (transform.position.z + transform.localScale.z)));
+			}
+			if ((pot.transform.position.x < transform.position.x) && (pot.transform.position.z < transform.position.z)) {
+				terrainRender(terrain10, ("terrain_" + (parameterX - 1).ToString () + "_" + (parameterZ - 1).ToString ()), new Vector3 ((transform.position.x - transform.localScale.x), 0, (transform.position.z - transform.localScale.z)));
+			}
+		}
+
+		//Destroy
 		/*if (dist > maxDist) {
 			Destroy (this.gameObject);
-		}*/
+		} */
+		if ((pot.transform.position.x > (transform.position.x + transform.localScale.x)) || (pot.transform.position.x < (transform.position.x - transform.localScale.x)) || (pot.transform.position.z > (transform.position.z + transform.localScale.z)) || (pot.transform.position.z < (transform.position.z - transform.localScale.z))) {
+			Destroy (this.gameObject);
+		}
 	}
 
 	void terrainRender(GameObject terrain, string terrainName, Vector3 terrainPosition) {
