6d38e599bb885af44ea388467d2dc129be0c5777
Jason Wong <jasonkwh@gmail.com>
2016-09-13 22:30:08 +1000

    Working on bird + nest

---
+++ Assets/Scripts/DestroyEgg.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyEgg : DestroySphere {
+
+	protected override void OnCollisionEnter (Collision col) {
+		if (col.gameObject.name == "potv2") {
+			GetComponent<MeshRenderer> ().enabled = false;
+			GetComponent<Rigidbody> ().useGravity = false;
+			GetComponent<Collider> ().enabled = false;
+			if (smoke.activeSelf == false) {
+				pot.GetComponent<Player> ().ate = true;
+			}
+			Destroy(transform.parent.gameObject); //delete nest
+			Destroy (this.gameObject);
+		}
+	}
+}
