bc4ae1fb7f8a8a3470bdb68f0a8c2d3be464085f
Jason Wong <jasonkwh@gmail.com>
2016-09-14 16:17:50 +1000

    Update Nest

--- Assets/Scripts/AiLittleDuck.cs
+++ Assets/Scripts/AiLittleDuck.cs
@@ -4,7 +4,18 @@ using System.Collections;
 public class AiLittleDuck : AiDuck {
 
 	protected override void FixedUpdate() {
-		
+		time += Time.deltaTime;
+		duckRotate();
+
+		if (dist < bounceRange) {
+			//potTransform.position += transform.forward * moveSpeed * Time.deltaTime;
+			potTransform.GetComponent<Player> ().pX = potTransform.position.x - transform.position.x;
+			potTransform.GetComponent<Player> ().pZ = potTransform.position.z - transform.position.z;
+			potTransform.GetComponent<Player> ().pushing = true;
+			potTransform.GetComponent<Player> ().stopBackup = false;
+			eBar.valueCurrent = eBar.valueCurrent - damage; //set energy bar value
+			backupTime = time;
+		}
 	}
 
 }
