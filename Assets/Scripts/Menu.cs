d9a5c9c00bf6f546af35265d3eed1712c6bb81aa
Jason Wong <jasonkwh@gmail.com>
2016-10-05 14:49:55 +1100

    Added Simple UI

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+//script for ui behaviors
+
+	public GameObject pause;
+	public GameObject menu;
+
+	void Start() {
+		menu.active = false;
+	}
+
+	public void clickPause() {
+		Time.timeScale = 0f;
+		pause.active = false;
+		menu.active = true;
+	}
+
+	public void clickResume() {
+		Time.timeScale = 1f;
+		menu.active = false;
+		pause.active = true;
+	}
+
+	public void clickRestart() {
+		Time.timeScale = 1f;
+		SceneManager.LoadScene("PotWalkv2", LoadSceneMode.Single);
+	}
+}
