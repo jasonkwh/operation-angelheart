53ae23cc55a489f833b90f870dbd6661d11e46c0
Chisanuphong Thammanukitcharoen <chisaunphong_2@hotmail.com>
2016-11-05 22:37:18 +1100

    fixed achievement logics

--- Assets/Scripts/achievement.cs
+++ Assets/Scripts/achievement.cs
@@ -24,11 +24,11 @@ public class achievement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(PlayerPrefs.GetInt("level") > 13) { //complete all forest levels
+		if(PlayerPrefs.GetInt("level") >= 13) { //complete all forest levels
 			achievement1.GetComponent<Toggle>().isOn = true;
-		} else if(PlayerPrefs.GetInt("level") > 23) { //complete all farm levels
+		} if(PlayerPrefs.GetInt("level") >= 23) { //complete all farm levels
 			achievement2.GetComponent<Toggle>().isOn = true;
-		} else if(PlayerPrefs.GetInt("level") > 33) { //complete the whole game
+		} if(PlayerPrefs.GetInt("level") >= 33) { //complete the whole game
 			achievement3.GetComponent<Toggle>().isOn = true;
 		}
 		if(PlayerPrefs.GetInt("playerBfs00") + PlayerPrefs.GetInt("playerBfs11") + PlayerPrefs.GetInt("playerBfs12") + PlayerPrefs.GetInt("playerBfs13") + PlayerPrefs.GetInt("playerBfs21") + PlayerPrefs.GetInt("playerBfs22") + PlayerPrefs.GetInt("playerBfs23") + PlayerPrefs.GetInt("playerBfs31") + PlayerPrefs.GetInt("playerBfs32") + PlayerPrefs.GetInt("playerBfs33") == 30) {
